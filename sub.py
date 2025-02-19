bdc0a9a9e04d9ba671f88cfac82616c96e8139fc
xujw3 <kingxujw3@gmail.com>
2025-02-19 14:53:56 +0800

    Create sub.py

---
+++ sub.py
@@ -0,0 +1,64 @@
+import requests
+
+
+def postdata(data):
+    json_data = {
+        'name': 'otc',
+        'displayName': '',
+        'form': '',
+        'mergeSources': '',
+        'ignoreFailedRemoteSub': False,
+        'icon': 'https://raw.githubusercontent.com/cc63/ICON/main/icons/ClashMeta.png',
+        'process': [
+            {
+                'type': 'Quick Setting Operator',
+                'args': {
+                    'useless': 'DISABLED',
+                    'udp': 'DEFAULT',
+                    'scert': 'DEFAULT',
+                    'tfo': 'DEFAULT',
+                    'vmess aead': 'DEFAULT',
+                },
+            },
+        ],
+        'tag': [
+            '第三方',
+        ],
+        'source': 'remote',
+        'url': data,
+        'content': '',
+        'ua': '',
+        'subscriptionTags': [],
+        'display-name': '',
+    }
+
+    response = requests.patch(
+        'https://087iu0np-substore.hf.space/793652832/api/sub/otc',
+        json=json_data,
+    )
+
+    return response
+
+
+def getdata(file_path):
+    sub_list = []
+    in_sub_list = False
+
+    with open(file_path, 'r', encoding='utf-8') as f:
+        for line in f:
+            stripped_line = line.strip()
+
+            if stripped_line == '-- sub_list --':
+                in_sub_list = True
+            elif stripped_line.startswith('--') and in_sub_list:
+                break  # 遇到下一个段落，停止提取
+            elif in_sub_list and stripped_line:
+                sub_list.append(stripped_line)
+
+    # 使用 '\n' 作为分隔符
+    return '\n'.join(sub_list)
+
+
+if __name__ == "__main__":
+    path = r"C:\Users\kjpmc21\Desktop\jm\config_sub_store.txt"
+    print(postdata(getdata(path)).text)
