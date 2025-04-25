0c1166026d8628f779d967052cc91d0cd90fe923
xujw3 <kingxujw3@gmail.com>
2025-04-25 13:38:20 +0800

    Update sub.py

--- sub.py
+++ sub.py
@@ -32,7 +32,7 @@ def postdata(data):
                 'disabled': False,
             },
         ],
-        'subUserinfo': 'upload=1000000000000; download=1000000000000; total=100000000000000; expire=4115721600; reset_day=1; plan_name=VIP1; app_url=https://087iu0np-substore.hf.space',
+        'subUserinfo': 'upload=1000000000000; download=1000000000000; total=100000000000000; expire=4115721600; reset_day=1; plan_name=VIP1; app_url=https://sub.xujw.dpdns.org',
         'proxy': '',
         'tag': [
             '第三方',
